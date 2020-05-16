4c96b310f9570b07976adeba1836c38a1bef64ef
Duc Thien Bui <thienbui797@gmail.com>
2020-05-17 09:12:27 +1000

    Query testing service (#2)

--- query-integrity-core/src/main/java/QueryFactory.java
+++ query-integrity-core/src/main/java/QueryFactory.java
@@ -2,8 +2,9 @@
  * Query Factory create query for a query test.
  *
  * @param <T> class of query used to the system under test.
+ * @param <R> class of result used for the system under test.
  */
-public interface QueryFactory<T> {
+public interface QueryFactory<T, R> {
     /**
      * Build criteria for querying/searching system under test.
      *
@@ -19,4 +20,22 @@ public interface QueryFactory<T> {
      * @return query for system under test.
      */
     public T build(QueryProxy<T> queryProxy);
+
+    /**
+     * Convert query to result.
+     *
+     * @param query the query to be used to get the result.
+     * @return the result of input query.
+     */
+    public R getResult(T query);
+
+    /**
+     * Convert result or query to readable string
+     *
+     * @param resultOrQuery a result or query needed to be converted.
+     * @return human readable string of input object.
+     */
+    public default String toString(Object resultOrQuery) {
+        return resultOrQuery.toString();
+    }
 }
