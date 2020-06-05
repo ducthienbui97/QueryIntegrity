ef31037d7b1476fb8b29eebba4d478db42f5c858
Thien Bui <thienbui797@gmail.com>
2020-06-05 22:33:35 +1000

    split test files

--- mongodb-integration/src/test/java/io/github/ducthienbui97/queryintegrity/mongodb/MongoDBQueryTestingCLIMongoConnectionTest.java
+++ mongodb-integration/src/test/java/io/github/ducthienbui97/queryintegrity/mongodb/MongoDBQueryTestingCLIMongoConnectionTest.java
@@ -4,23 +4,23 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
-import io.github.ducthienbui97.queryintegrity.core.QueryTestingService;
+import de.bwaldvogel.mongo.wire.MongoWireProtocolHandler;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class MongoDBQueryTestingCLIRunningTest extends MongoDBQueryTestingCLITest {
-
+public class MongoDBQueryTestingCLIMongoConnectionTest extends MongoDBQueryTestingCLITest {
     @ParameterizedTest()
     @MethodSource("io.github.ducthienbui97.queryintegrity.mongodb.MongoDBQueryTestingCLITest#parameterFormats")
-    public void testRunAllTests(String database, String collection, String connectionString,
-                                String configFile, String notTest, String equalTest, String subsetTest) {
-        Logger logger = (Logger) LoggerFactory.getLogger(QueryTestingService.class);
+    public void testMongoServerConnected(String database, String collection, String connectionString,
+                                         String configFile, String notTest, String equalTest, String subsetTest) {
+        Logger logger = (Logger) LoggerFactory.getLogger(MongoWireProtocolHandler.class);
         logger.setLevel(Level.INFO);
         ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
         listAppender.start();
@@ -30,13 +30,9 @@ public class MongoDBQueryTestingCLIRunningTest extends MongoDBQueryTestingCLITes
                 collection, COLLECTION_NAME,
                 connectionString, mongoURI,
                 configFile, jsonFilePath,
-                equalTest, subsetTest, notTest
-        });
+                equalTest, subsetTest, notTest});
         List<ILoggingEvent> logsList = listAppender.list;
-        assertThat(logsList, allOf(
-                hasItem(loggingEventMatcher(containsString("Not test"))),
-                hasItem(loggingEventMatcher(containsString("Equal test"))),
-                hasItem(loggingEventMatcher(containsString("Subset test")))));
+        assertThat(logsList, hasItem(loggingEventMatcher(containsString("connected"))));
         logger.setLevel(Level.OFF);
     }
 }
